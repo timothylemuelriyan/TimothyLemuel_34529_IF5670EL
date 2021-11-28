d3ea7fd9b9cc2c41e9fdcb941ea40550914e9aa7
Timothy Lemuel Riyan <90091804+timothylemuelriyan@users.noreply.github.com>
2021-11-28 13:16:06 +0700

    Add files via upload

---
+++ Week10C_34529/app/src/main/java/umn/ac/id/week10c_34529/MainActivity.java
@@ -0,0 +1,56 @@
+package umn.ac.id.week10c_34529;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    CustomBoundService customBoundService;
+    boolean isBound = false;
+    private ServiceConnection serviceConnection =
+            new ServiceConnection() {
+                @Override
+                public void onServiceConnected(ComponentName name,
+                                               IBinder service) {
+                    CustomBoundService.CustomLocalBinder binder =
+                            (CustomBoundService.CustomLocalBinder) service;
+                    customBoundService = binder.getService();
+                    isBound = true;
+                }
+
+                @Override
+                public void onServiceDisconnected(ComponentName name) {
+                    isBound = false;
+                }
+            };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent servIntent = new Intent(this, SimpleIntentService.class);
+        startService(servIntent);
+        Button btnStartService = findViewById(R.id.main_button_startservice);
+        btnStartService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,
+                        CustomService.class);
+                startService(intent);
+            }
+        });
+        Button btnShowTime = findViewById(R.id.main_button_showtime);
+        btnShowTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String currentTime = customBoundService.getCurrentTime();
+                Toast.makeText(getApplicationContext(),
+                        currentTime,Toast.LENGTH_LONG).show();
+            }
+        });;
+
+        Intent intent2 = new Intent(this, CustomBoundService.class);
+        bindService(intent2, serviceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+}
